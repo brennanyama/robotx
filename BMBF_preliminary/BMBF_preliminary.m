813da2fea4ce4dd78e8de0671e8d129db10ff4d7
brennanyama <brennanyama@gmail.com>
2016-09-21 16:59:42 -1000

    Add files via upload

---
+++ BMBF_preliminary/BMBF_preliminary.m
@@ -0,0 +1,16 @@
+function BMBF_preliminary
+
+    % Get location information (current position (x,y), current heading
+    % (twist(=))
+    
+    % Look at camera to determine where the targets are
+    
+        % If you can see the targets, move on
+        % If you can't see the targets, spin
+    
+    % Calculate heading realitve to the mean (middle) of the targets
+    
+    % Drive forward at constant velocity (maybe 50% or so), and control
+    % heading (via PWM) to targets
+
+end
