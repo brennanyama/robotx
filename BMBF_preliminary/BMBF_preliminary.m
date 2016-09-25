2e7de53e0a5788bd5a2aa62bdb33c3348756027c
brennanyama <brennanyama@gmail.com>
2016-09-25 03:36:58 -1000

    Update BMBF_preliminary.m

--- BMBF_preliminary/BMBF_preliminary.m
+++ BMBF_preliminary/BMBF_preliminary.m
@@ -1,15 +1,18 @@
 function BMBF_preliminary
 
-    % Get location information (current position (x,y), and current heading (psi)
+    % This code is making the assumption that the LIDAR unit will not be available
+
+    % Get location information (current position (x,y), and current heading (psi))
     
-    % Look at camera to determine where the targets are
+    % Engage target search behavior
+        % Look through camera
+            % If you can see the targets, move on
+            % If you can't see the targets, spin
     
-        % If you can see the targets, move on
-        % If you can't see the targets, spin
+    % Engage target follow behavior
+        % Drive forward at constant velocity
     
-    % Calculate heading realitve to the mean (middle) of the targets
-    
-    % Drive forward at constant velocity (maybe 50% or so), and control
-    % heading (via PWM) to targets
+        % Calculate heading relative to the mean (middle) of the targets, dpsi.  This represents your error function.
+        % Update controller gains as a result of heading error
 
 end
