694a5480e5669707931eb37c2b3cffb15f5fd38e
igek <igek@hawaii.edu>
2016-10-20 10:09:17 -1000

    Added GoalX and GoalY
    Subscribed to the GoalX and GoalY topics from the Mission Planner node as well as input the coordinates received from the mission planner into the D* algorithm.
    
    TODO: receive current position location from ROS and use the coordinates as the start position.

--- pathplanning/Version_02/dstar.m
+++ pathplanning/Version_02/dstar.m
@@ -7,7 +7,8 @@
 % Sub/Pub/Msg
 OGridSub = rossubscriber('/map', 'nav_msgs/OccupancyGrid');
 GPSSub = rossubscriber('/GPSLocation', 'sensor_msgs/NavSatFix');
-GoalSub = rossubscriber('/GoalCoordinates', 'GOAL MSGTYPE HERE');
+GoalX = rossubscriber('/GoalX', 'std_msgs/Int16');
+GoalY = rossubscriber('/GoalY', 'std_msgs/Int16');
 OptimalPathxPub = rospublisher('/pathX', 'std_msgs/Int16MultiArray');
 OptimalPathyPub = rospublisher('/pathY', 'std_msgs/Int16MultiArray');
 PathxMsg = rosmessage(OptimalPathxPub);
@@ -16,12 +17,13 @@ PathyMsg = rosmessage(OptimalPathyPub);
 % Receive Occupancy grid, gps, goal, and start information from ROS
 OGridData = receive(OGridSub, 3);       
 GPSData = receive(GPSSub, 3);           % GPSData = GPSSub.LatestMessage;
-GoalData = receive(GoalSub, 3);         
+gX = receive(GoalX, 3);
+gY = receive(GoalY, 3);
 
 
 % Create a D* Object using the map
 map = convO2M(OGridData);
-goal = [10, 10];          % TODO: receive goal and start from ROS
+goal = [gX, gY];          % TODO: receive start from ROS
 start=[0, 0];
 ds = Dstar(map);
 ds.plan(goal);
