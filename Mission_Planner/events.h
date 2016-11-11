4db5f9ce39592ec6457ed05815b1385e029c62ce
Paulo Lemus <paulolemus14@gmail.com>
2016-11-11 13:00:44 -1000

    G collection

--- Mission_Planner/events.h
+++
@@ -1,9 +0,0 @@
-#ifndef MISSION_PLANNER_EVENTS_H
-#define MISSION_PLANNER_EVENTS_H
-
-void obstable(int, int);
-void manual();
-void killSwitch();
-void eventAlert(const std_msgs::String::ConstPtr&);
-
-#endif //MISSION_PLANNER_EVENTS_H
