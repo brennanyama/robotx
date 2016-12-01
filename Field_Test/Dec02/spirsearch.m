f6277039c626ee66bec0b03bbeb4daa648104b82
akwong7 <akwong7@hawaii.edu>
2016-12-01 11:19:04 -1000

    Changed spirsearch.m
    The search patter that is output by this function now spirals inwards to a point defined by (addX, addY) that is relative to the robot's current position.

--- Field_Test/Dec02/spirsearch.m
+++ Field_Test/Dec02/spirsearch.m
@@ -1,8 +1,10 @@
 function [x, y] = spirsearch(R, turns, currX, currY);
+    addX = 5;               % Distance in meters in front of the robot to center search aroud
+    addY = 0;               
     theta = [-1*pi*turns: 0.1 : pi*turns];
-    r = 0:R/(length(theta)-1):R;
-    x = r.*sin(theta);
-    x = x+currX;
-    y = r.*cos(theta);
-    y = y+currY;
+    r = R:-(R-0.5)/(length(theta)-1):0.5;
+    x = r.*cos(theta);
+    x = x+currX+addX;
+    y = r.*sin(theta);
+    y = y+currY+addY;
 end
