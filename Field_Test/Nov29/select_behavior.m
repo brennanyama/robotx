e99229f90b56a2077fd504437daf22ad0bee9633
akwong7 <akwong7@hawaii.edu>
2016-11-30 17:41:45 -1000

    New Behaviors added

--- Field_Test/Nov29/select_behavior.m
+++ Field_Test/Nov29/select_behavior.m
@@ -1,17 +1,29 @@
 function behavior = select_behavior(k,x,control_tolerances,error,behavior)
-%     
-%     if error(4,k) > control_tolerances(1)                                   % robot is not within goal radius tolerance
-%         if abs(error(5,k)) > control_tolerances(2)                          % robot is not on within path heading tolerance
-%             behavior(1,k) = 1;
-%         else
-%             behavior(1,k) = 2;
-%         end
-%     else                                                                    % robot is within goal radius tolerance
-%         if abs(x(4,k)) || abs(x(5,k)) > control_tolerances(4)               % drive velocity to zero
-%             behavior(1,k) = 3;
-%         else
-%             behavior(1,k) = 4;
-%         end
-%     end
-    behavior(1, k) = 6;
-end
+
+%% Waypoint Navigation Code
+
+    if (error(4,k) >= control_tolerances(1))                % robot is outside the approach radius
+        if (error(5,k) >= control_tolerances(3))            % robot is outside path heading tolerance
+            behavior(1,k) = 1;                                  % control psi to path heading and control u to medium fast
+        else                                                % robot is inside path heading tolerance
+            behavior(1,k) = 2;                                  % control psi to path heading and control u to fast
+        end
+    else                                                    % robot is inside approach radius
+        if (error(4,k) >= control_tolerances(2))            % robot is outside in the goal radius
+            if (error(5,k) >= control_tolerances(3))        % robot is outside path heading tolerance
+                behavior(1,k) = 3;                              % control psi to path heading and control u to slow
+            else                                            % robot is inside path heading tolerance
+                behavior(1,k) = 4;                              % control psi to path heading and control u to medium slow
+            end
+        else                                                % robot is inside goal radius
+            if (x(4,k) >= 0.25)                             % robot is moving
+                behavior(1,k) = 5;                              % control u to 0 m/s
+            else                                            % robot is largely stopped
+                if (error(3,k) >= control_tolerances(4))    % robot is not facing goal heading
+                    behavior(1,k) = 6;                          % control psi to goal heading and control u to 0 m/s
+                else                                        % robot is facing goal heading
+                    behavior(1,k) = 7;                          % station keep
+                end
+            end
+        end
+    end
