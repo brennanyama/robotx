0ab59dc22ac36e69915827f86801c64015b60a24
akwong7 <akwong7@hawaii.edu>
2016-11-30 17:37:07 -1000

    Update Error calculation
    HO FUCK equation added. For more information, ask Brennan Yamamoto.

--- Field_Test/Nov29/update_error.m
+++ Field_Test/Nov29/update_error.m
@@ -1,18 +1,19 @@
-function error_col = update_error(k,x,meas,goal_vars,control_maximums)
-    temp = zeros(9, 1);
+function error = update_error(k,x,meas,goal_vars,error)
+
     % Update errors
-    temp(1,1) = goal_vars(1)-x(1,k);                   % x position goal error
-    temp(2,1) = goal_vars(2)-x(2,k);                   % y position goal error
-    temp(3,1) = goal_vars(3)-x(3,k);                   % psi heading goal error
-    temp(4,1) = sqrt((goal_vars(1)-x(1,k))^2+...       % path distance error
+    error(1,k) = goal_vars(1)-x(1,k);                   % x position goal error
+    error(2,k) = goal_vars(2)-x(2,k);                   % y position goal error
+    error(3,k) = goal_vars(3)-x(3,k);                   % psi heading goal error
+    error(4,k) = sqrt((goal_vars(1)-x(1,k))^2+...       % path distance error
         (goal_vars(2)-x(2,k))^2);   
-    temp(5,1) = atan2(goal_vars(2)-x(2,k),...          % path heading error
-         goal_vars(1)-x(1,k))-x(3,k);
-%     error(5,k) = atan2(goal_vars(2)-x(2,k),...          % path heading error
-%         goal_vars(1)-x(1,k));
-    temp(6,1) = control_maximums(1,1)-meas(4,k);         % surge velocity max speed error
-    temp(7,1) = control_maximums(1,2)-meas(5,k);         % sway velocity max speed error
-    temp(8,1) = 0-meas(4,k);                           % surge velocity stop error
-    temp(9,1) = 0-meas(5,k);                           % sway velocity stop error    
-    error_col = temp;
+    error(5,k) = atan2(sin(atan2(goal_vars(2)-x(2,k),...    % HO FUCK EQUATION (there's a good story behind this one)
+        goal_vars(1)-x(1,k))-x(3,k)),...
+        cos(atan2(goal_vars(2)-x(2,k),...
+        goal_vars(1)-x(1,k))-x(3,k)));
+    error(6,k) = 0-x(4,k);                              % surge velocity stop error   
+    error(7,k) = 0.10-x(4,k);                           % surge velocity slow error
+    error(8,k) = 0.25-x(4,k);                           % surge velocity medium slow m/s error
+    error(9,k) = 0.5-x(4,k);                            % surge velocity medium fast m/s error
+    error(10,k) = 2.00-x(4,k);                          % surge velocity fast m/s error
+    
 end
